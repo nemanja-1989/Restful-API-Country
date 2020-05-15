94d4762fd2029e164b1670fb86e05cd7f7bba8f9
Nemanja-1989 <nemanja_jocic@yahoo.com>
2020-05-15 08:52:23 +0200

    Modified CountryController and create route for CountryController

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -18,16 +18,6 @@ class CountryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -45,17 +35,6 @@ class CountryController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Country $country)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Country  $country
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Country $country)
     {
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Create Api resource for CountryController
+Route::apiResource("countries", "CountryController");
