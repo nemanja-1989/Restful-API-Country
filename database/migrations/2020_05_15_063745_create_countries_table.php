5852ee91be9cd313749c23d10c838ad84ce96a37
Nemanja-1989 <nemanja_jocic@yahoo.com>
2020-05-15 08:44:25 +0200

    Modified Country model and run country migration

--- database/migrations/2020_05_15_063745_create_countries_table.php
+++ database/migrations/2020_05_15_063745_create_countries_table.php
@@ -15,6 +15,8 @@ class CreateCountriesTable extends Migration
     {
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
+            $table->string("name", "45");
+            $table->string("image");
             $table->timestamps();
         });
     }
